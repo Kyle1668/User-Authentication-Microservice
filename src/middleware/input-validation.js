67dc2d7e7cec1e732ebe9515df1bf5f59b46a9fa
Kyle1668 <kyledevinobrien1@gmail.com>
2018-07-18 11:47:01 -0700

    progress

--- src/middleware/input-validation.js
+++ src/middleware/input-validation.js
@@ -1,17 +1,17 @@
 const inputValidation = (req, res, next) => {
 	console.log('Middleware Ran');
+	console.log(req.query);
 
 	if (req.query.email && req.query.password) {
-		console(req.query);
 		next();
+	} else {
+		res.json({
+			code: 400,
+			message: 'Missing Arguments',
+			token: null,
+			error: true
+		});
 	}
-
-	res.json({
-		code: 400,
-		message: 'Missing Arguments',
-		token: null,
-		error: true
-	});
 };
 
 module.exports.inputValidation = inputValidation;
