440cdca589cd60fe2ce65bafbf1919ec51740de3
Kyle1668 <kyledevinobrien1@gmail.com>
2018-07-23 10:30:03 -0700

    removed example code

--- src/models/redis-connection.js
+++ src/models/redis-connection.js
@@ -9,10 +9,4 @@ const host = process.eventNames.REDIS_HOST || '127.0.0.1';
 const port = process.env.REDIS_PORT || 6379;
 const redisConnection = redis.createClient(port, host);
 
-redisConnection.getAsync('test').then((result) => {
-    console.log(result);
-}).catch((err) => {
-    console.log(err);
-});
-
 module.exports = { redisConnection };
